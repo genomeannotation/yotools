fc41d7c125a7965a77953ebd79ca12aeb3743a43
Theodore DeRego <tderego94@gmail.com>
2015-02-06 14:14:26 -1000

    Bases::reverse_complement

--- src/bases.rs
+++ src/bases.rs
@@ -8,6 +8,26 @@ impl Bases {
         Bases(bases.to_string())
     }
 
+    /// Reverse complements the sequence
+    pub fn reverse_complement(&mut self) {
+        let &mut Bases(ref mut bases) = self;
+        *bases = bases.as_slice().chars().rev().map(
+            |b| {
+                match b {
+                    'A' => 'T',
+                    'a' => 't',
+                    'T' => 'A',
+                    't' => 'a',
+                    'G' => 'C',
+                    'g' => 'c',
+                    'C' => 'G',
+                    'c' => 'g',
+                    _ => unreachable!(),
+                }
+            }
+        ).collect();
+    }
+
     /// Tries to debarcode the sequence
     /// On success returns debarcoded sequence
     /// On failure returns original sequence
@@ -20,7 +40,16 @@ impl Bases {
 // Unit tests
 
 #[test]
-fn test_debarcode_works_properly() {
+fn reverse_complement() {
+    let mut bases = Bases::from_str("GATACA");
+    
+    bases.reverse_complement();
+
+    assert_eq!(bases, Bases::from_str("TGTATC"));
+}
+
+#[test]
+fn debarcode_works_properly() {
     let bases = Bases::from_str("ATAGGATACACTAT");
 
     let debarcoded = bases.debarcode(0);
@@ -29,7 +58,7 @@ fn test_debarcode_works_properly() {
 }
 
 #[test]
-fn test_debarcode_works_properly_with_diff() {
+fn debarcode_works_properly_with_diff() {
     let bases = Bases::from_str("ATTGGATACATTAT");
 
     let debarcoded = bases.debarcode(1);
@@ -38,7 +67,7 @@ fn test_debarcode_works_properly_with_diff() {
 }
 
 #[test]
-fn test_debarcode_fails_properly() {
+fn debarcode_fails_properly() {
     let bases = Bases::from_str("ATTGGATACACTAT");
 
     let debarcoded = bases.debarcode(0);
