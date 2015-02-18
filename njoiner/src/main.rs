5b3c3ef50a215ffad7f40e976f90ccd4141bbc70
Theodore DeRego <tderego94@gmail.com>
2015-02-17 16:21:41 -1000

    Started njoiner

--- njoiner/src/main.rs
+++ njoiner/src/main.rs
@@ -1,3 +1,18 @@
+extern crate bio;
+
+use std::collections::HashMap;
+use std::collections::hash_map::Entry;
+use std::old_io::{
+    BufferedReader,
+    File,
+};
+
+use bio::fastq;
+
 fn main() {
-    println!("Hello, world!");
+    let mut forward_fastq = BufferedReader::new(File::open(&Path::new("forward.fastq")));
+    let mut reverse_fastq = BufferedReader::new(File::open(&Path::new("reverse.fastq")));
+    
+    let forward_seqs = fastq::read_fastq(&mut forward_fastq);
+    let reverse_seqs = fastq::read_fastq(&mut reverse_fastq);
 }
