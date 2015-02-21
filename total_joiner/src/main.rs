6c09f1aa9a34d792a24562ddcae8d85483ba99e0
Theodore DeRego <tderego94@gmail.com>
2015-02-20 16:15:19 -1000

    Copied fastq i/o from njoiner

--- total_joiner/src/main.rs
+++ total_joiner/src/main.rs
@@ -1,5 +1,29 @@
 extern crate bio;
 
+use std::collections::HashMap;
+use std::collections::hash_map::Entry;
+use std::old_io::{
+    BufferedReader,
+    BufferedWriter,
+    File,
+};
+
+use bio::bases;
+use bio::fastq;
+
 fn main() {
-    println!("Hello, world!");
+    let mut forward_fastq = BufferedReader::new(File::open(&Path::new("forward.fastq")));
+    let mut reverse_fastq = BufferedReader::new(File::open(&Path::new("reverse.fastq")));
+    
+    let forward_seqs = fastq::read_fastq(&mut forward_fastq);
+    let reverse_seqs = fastq::read_fastq(&mut reverse_fastq);
+
+    let mut joined_seqs = vec!();
+
+    for (forward_seq, reverse_seq) in forward_seqs.into_iter().zip(reverse_seqs.into_iter()) {
+    }
+
+    let mut sorted_fastq = BufferedWriter::new(File::create(&Path::new("joined.fastq"))); 
+
+    fastq::write_fastq(&mut sorted_fastq, joined_seqs.iter());
 }
