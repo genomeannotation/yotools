f769bc2b4b6a4fdb84d40a6a108a84f1c2360873
Theodore DeRego <tderego94@gmail.com>
2015-04-23 14:49:44 -1000

    call_consensus builds properly

--- call_consensus/src/main.rs
+++ call_consensus/src/main.rs
@@ -1,25 +1,29 @@
 #![feature(io)]
 #![feature(path)]
+#![feature(core)]
 
 extern crate bio;
 
 use std::cmp;
 use std::collections::HashMap;
 use std::collections::hash_map::Entry;
-use std::old_io::{
-    BufferedReader,
-    BufferedWriter,
-    File,
+use std::fs::{File, OpenOptions};
+use std::io::{
+    BufRead,
+    BufReader,
+    BufWriter,
+    Write,
 };
+use std::path::Path;
 
 use bio::bases;
 use bio::fastq;
 
 fn main() {
-    let mut fastq_file = BufferedReader::new(File::open(&Path::new("sorted.fastq")));
+    let mut fastq_file = BufReader::new(File::open(&Path::new("sorted.fastq")).unwrap());
 
-    let mut samples_file = BufferedReader::new(File::open(&Path::new("samples")));
-    let mut loci_file = BufferedReader::new(File::open(&Path::new("loci")));
+    let mut samples_file = BufReader::new(File::open(&Path::new("samples")).unwrap());
+    let mut loci_file = BufReader::new(File::open(&Path::new("loci")).unwrap());
     
     // Read fastq file
     let seqs = fastq::read_fastq(&mut fastq_file);
@@ -51,36 +55,37 @@ fn main() {
         let sample = seq.header.as_slice().split(' ').nth(1).unwrap().split(':').nth(3).unwrap().to_string();
         let loci = seq.header.as_slice().split(' ').nth(3).unwrap().trim_right().to_string();
 
-        seq_matrix[loci][sample].push(seq);
+        seq_matrix.get_mut(&loci).unwrap().get_mut(&sample).unwrap().push(seq);
     }
 
-    // Unique-ify sequence piles
+    // Call consensus
     println!("Calling consensus...");
-    let mut consensus_file = BufferedWriter::new(File::create(&Path::new("consensus.tsv"))); 
-    let mut consensus_matrix = BufferedWriter::new(File::create(&Path::new("yay_nay_matrix.tsv"))); 
-    let mut count_matrix = BufferedWriter::new(File::create(&Path::new("counts_matrix.tsv"))); 
+
+    let mut consensus_file = BufWriter::new(File::create(&Path::new("consensus.tsv")).unwrap()); 
+    let mut consensus_matrix = BufWriter::new(File::create(&Path::new("yay_nay_matrix.tsv")).unwrap()); 
+    let mut count_matrix = BufWriter::new(File::create(&Path::new("counts_matrix.tsv")).unwrap()); 
     
-    let mut fasta1_file = BufferedWriter::new(File::create(&Path::new("1.fasta"))); 
-    let mut fasta2_file = BufferedWriter::new(File::create(&Path::new("2.fasta"))); 
-    let mut fasta3_file = BufferedWriter::new(File::create(&Path::new("3.fasta"))); 
-    let mut fasta4_file = BufferedWriter::new(File::create(&Path::new("4.fasta"))); 
+    let mut fasta1_file = BufWriter::new(File::create(&Path::new("1.fasta")).unwrap()); 
+    let mut fasta2_file = BufWriter::new(File::create(&Path::new("2.fasta")).unwrap()); 
+    let mut fasta3_file = BufWriter::new(File::create(&Path::new("3.fasta")).unwrap()); 
+    let mut fasta4_file = BufWriter::new(File::create(&Path::new("4.fasta")).unwrap()); 
 
     // Write column labels to matrices
     let samples_row = "\t".to_string() + &samples.connect("\t") + "\n";
-    consensus_matrix.write_str(samples_row.as_slice());
-    count_matrix.write_str(samples_row.as_slice());
+    consensus_matrix.write_all(samples_row.as_bytes());
+    count_matrix.write_all(samples_row.as_bytes());
 
     for (loci, sample_map) in &seq_matrix {
         // Begin rows of matrices
-        consensus_matrix.write_str(loci.as_slice());
-        count_matrix.write_str(loci.as_slice());
+        consensus_matrix.write_all(loci.as_bytes());
+        count_matrix.write_all(loci.as_bytes());
 
         for (sample, seqs) in sample_map {
             // Output count matrix entry
-            count_matrix.write_str(format!("\t{}", seqs.len()).as_slice());
+            count_matrix.write_all(format!("\t{}", seqs.len()).as_bytes());
 
             if seqs.len() < 42 {
-                consensus_matrix.write_str("\t0");
+                consensus_matrix.write_all(b"\t0");
                 continue;
             } 
 
@@ -101,44 +106,44 @@ fn main() {
                 seq_counts.sort_by(|&(_, a), &(_, b)| a.cmp(&b));
                 seq_counts.reverse();
 
-                consensus_matrix.write_str(format!("\t{}", cmp::min(4, seq_counts.len())).as_slice());
+                consensus_matrix.write_all(format!("\t{}", cmp::min(4, seq_counts.len())).as_bytes());
 
                 // Write fastas
                 if seq_counts.len() > 0 {
                     let &(ref bases1, count1) = &seq_counts[0];
-                    fasta1_file.write_str(format!(">{}_{} {}\n{}\n", loci, sample, count1, bases1.as_string()).as_slice());
+                    fasta1_file.write_all(format!(">{}_{} {}\n{}\n", loci, sample, count1, bases1.as_string()).as_bytes());
                 }
 
                 if seq_counts.len() > 1 {
                     let &(ref bases2, count2) = &seq_counts[1];
-                    fasta2_file.write_str(format!(">{}_{} {}\n{}\n", loci, sample, count2, bases2.as_string()).as_slice());
+                    fasta2_file.write_all(format!(">{}_{} {}\n{}\n", loci, sample, count2, bases2.as_string()).as_bytes());
                 }
 
                 if seq_counts.len() > 2 {
                     let &(ref bases3, count3) = &seq_counts[2];
-                    fasta3_file.write_str(format!(">{}_{} {}\n{}\n", loci, sample, count3, bases3.as_string()).as_slice());
+                    fasta3_file.write_all(format!(">{}_{} {}\n{}\n", loci, sample, count3, bases3.as_string()).as_bytes());
                 }
 
                 if seq_counts.len() > 3 {
                     let &(ref bases4, count4) = &seq_counts[3];
-                    fasta4_file.write_str(format!(">{}_{} {}\n{}\n", loci, sample, count4, bases4.as_string()).as_slice());
+                    fasta4_file.write_all(format!(">{}_{} {}\n{}\n", loci, sample, count4, bases4.as_string()).as_bytes());
                 }
 
                 // Write consensus file stuff
-                consensus_file.write_str(format!("{}\t{}", loci, sample).as_slice());
+                consensus_file.write_all(format!("{}\t{}", loci, sample).as_bytes());
                 for &(_, count) in &seq_counts {
-                    consensus_file.write_str(format!("\t{}", count).as_slice());
+                    consensus_file.write_all(format!("\t{}", count).as_bytes());
                 }
-                consensus_file.write_str("\n");
+                consensus_file.write_all(b"\n");
                 for &(ref bases, _) in &seq_counts {
-                    consensus_file.write_str(format!("\n{}", bases.as_string()).as_slice());
+                    consensus_file.write_all(format!("\n{}", bases.as_string()).as_bytes());
                 }
-                consensus_file.write_str("\n\n");
+                consensus_file.write_all(b"\n\n");
             } else {
-                consensus_matrix.write_str("\t0");
+                consensus_matrix.write_all(b"\t0");
             }
         }
-        consensus_matrix.write_str("\n");
-        count_matrix.write_str("\n");
+        consensus_matrix.write_all(b"\n");
+        count_matrix.write_all(b"\n");
     }
 }
