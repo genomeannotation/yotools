ddbfddab7d9b7be30e40c8da1a02664d54bd23cd
Theodore DeRego <tderego94@gmail.com>
2015-06-19 10:22:42 -1000

    Finished, just need to polish interface

--- hetsep/src/main.rs
+++ hetsep/src/main.rs
@@ -1,10 +1,17 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{
+    BufRead,
+    BufReader,
+    BufWriter,
+    Write,
+};
 
 fn main() {
     // Read the fasta file
     let input_file = BufReader::new(File::open("file.fa").unwrap());
+
+    println!("Reading fasta...");
     
     let mut seqs: Vec<(String, String)> = Vec::new();
 
@@ -25,12 +32,50 @@ fn main() {
     }
 
     // Sort the sequences
+    println!("Sorting sequences...");
+
     let mut sorted: HashMap<String, Vec<String>> = HashMap::new();
 
     for &(ref header, ref bases) in &seqs {
         if header.as_bytes()[header.len() - 2] == b'_' {
             let seqs = sorted.entry(header[0..header.len() - 2].to_string()).or_insert(Vec::new());
+            // Make sure any new sequences are the right length
+            if let Some(seq) = seqs.get(0) {
+                if seq.len() != bases.len() {
+                    println!("WARNING: trashing sequence with different length: {}", header);
+                    break;
+                }
+            }
             seqs.push(bases.clone());
         }
+    }
+
+    // Output mixed base
+    println!("Writing output...");
+
+    let mut out_fasta = BufWriter::new(File::create("output.fa").unwrap());
+
+    for (header, het_seqs) in &sorted {
+        write!(&mut out_fasta, ">{}\n", header).unwrap();
+        for i in (0..het_seqs[0].len()) {
+            let mut base_variants = HashSet::new();
+            for seq in het_seqs {
+                base_variants.insert(seq.as_bytes()[i]);
+            }
+            if base_variants.len() == 1 {
+                let base = base_variants.iter().next().unwrap();
+                out_fasta.write_all(&[*base]).unwrap();
+            } else {
+                out_fasta.write_all(b"[").unwrap();
+                for (i, base) in base_variants.iter().enumerate() {
+                    if i != 0 {
+                        out_fasta.write_all(b", ").unwrap();
+                    }
+                    out_fasta.write_all(&[*base]).unwrap();
+                }
+                out_fasta.write_all(b"]").unwrap();
+            }
+        }
+        out_fasta.write_all(b"\n").unwrap();
     }
 }
